d82e1a13cf5b876e4cee915fa4d6ac0ab31086c6
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-15 10:45:26 +0200

    Lesson 30

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,8 +2,8 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add(num1, num2) {
-    const result = num1 + num2;
-    return result;
+  const result = num1 + num2;
+  return result;
 }
 
 currentResult = add(1, 2);
