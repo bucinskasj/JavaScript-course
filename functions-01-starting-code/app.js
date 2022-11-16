c7f0a188c680cbdb9c58c8df0674ff30e566f5b7
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-16 22:48:35 +0200

    Lesson 132

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,34 +1,59 @@
 const startGameBtn = document.getElementById("start-game-btn");
 
-const ROCK = 'ROCK';
-const PAPER = 'PAPER';
-const SCISSORS = 'SCISSORS';
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
 const DEFAULT_USER_CHOISE = ROCK;
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WON = "PLAYER_WON";
+const RESULT_COMPUTER_WON = "COMPUTER_WON";
 
 let gameIsRunning = false;
 
-
 const getPlayerChoice = function () {
-  const selection = prompt(`${ROCK}, ${PAPER}, ${SCISSORS}?`, '').toUpperCase();
-  if (
-    selection !== ROCK &&
-    selection !== PAPER &&
-    selection !== SCISSORS
-  ) {
+  const selection = prompt(`${ROCK}, ${PAPER}, ${SCISSORS}?`, "").toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
     alert(`Invalid choice! We chose ${DEFAULT_USER_CHOISE} for you!`);
     return DEFAULT_USER_CHOISE;
   }
   return selection;
 };
 
+const getComputerChoice = function () {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = function (cChoice, pChoice) {
+  if (cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (
+    cChoice === ROCK && pChoice === PAPER ||
+    cChoice === PAPER && pChoice === SCISSORS ||
+    cChoice === SCISSORS && pChoice === ROCK
+  ) {
+    return RESULT_PLAYER_WON;
+  } else {
+    return RESULT_COMPUTER_WON;
+  }
+};
+
 startGameBtn.addEventListener("click", function () {
-  if (gameIsRunning){
+  if (gameIsRunning) {
     return;
   }
   gameIsRunning = true;
   console.log("Games is starting...");
   const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerSelection);
+  console.log(winner);
 });
 
 // Method example
