99a353d188c3cf3403c153c69600cee286dd1836
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-16 14:12:17 +0200

    Lesson 103

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -15,6 +15,7 @@ const enteredValue = prompt("Maximum life for you and the monster.", "100");
 
 let chosenMaxLife = parseInt(enteredValue);
 let battleLog = [];
+let lastLoggedEntry;
 
 if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
   chosenMaxLife = 100;
@@ -254,12 +255,16 @@ function printLogHandler() {
   //   console.log(battleLog[i]);
   // }
 
-  // "For Of" loop to go through each element in the array
+  // "For Of" loop to go through each element in the array wtih some breaks
   let i = 0;
   for (const logEntry of  battleLog) {
-    console.log(`#${i}`);
-    for (const key in logEntry){
-      console.log(`${key} => ${logEntry[key]}`);
+    if (!lastLoggedEntry && lastLoggedEntry !== 0 || lastLoggedEntry < i) {
+      console.log(`#${i}`);
+      for (const key in logEntry){
+        console.log(`${key} => ${logEntry[key]}`);
+      }
+      lastLoggedEntry = i;
+      break;
     }
     i++;
   }
