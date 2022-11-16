50427d7ec0101cacde152aee2c88bb55d06bc083
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-16 22:33:56 +0200

    Lesson 131

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,18 +1,35 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
 
-startGameBtn.addEventListener('click', function(){
-  console.log('Games is starting...');
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOISE = ROCK;
+
+let gameIsRunning = false;
+
+
+const getPlayerChoice = function () {
+  const selection = prompt(`${ROCK}, ${PAPER}, ${SCISSORS}?`, '').toUpperCase();
+  if (
+    selection !== ROCK &&
+    selection !== PAPER &&
+    selection !== SCISSORS
+  ) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOISE} for you!`);
+    return DEFAULT_USER_CHOISE;
+  }
+  return selection;
+};
+
+startGameBtn.addEventListener("click", function () {
+  if (gameIsRunning){
+    return;
+  }
+  gameIsRunning = true;
+  console.log("Games is starting...");
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
 });
-
-
-
-
-
-
-
-
-
-
 
 // Method example
 // const person = {
