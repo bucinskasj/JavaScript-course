cd7061559bda13f15d9385e9a6dfac70b826c187
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-16 13:28:29 +0200

    Lesson 101

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -242,8 +242,10 @@ function printLogHandler() {
   // "For Of" loop to go through each element in the array
   let i = 0;
   for (const logEntry of  battleLog) {
-    console.log(logEntry);
-    console.log(i);
+    console.log(`#${i}`);
+    for (const key in logEntry){
+      console.log(`${key} => ${logEntry[key]}`);
+    }
     i++;
   }
 }
