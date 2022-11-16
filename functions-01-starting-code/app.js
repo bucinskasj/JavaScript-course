a2088050b6ce9722738f0c4eba7b86f5ca1831ad
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-16 21:25:16 +0200

    Lesson 126

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,1 +1,17 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+  console.log('Game is starting...');
+}
+
+startGameBtn.addEventListener('click', startGame);
+
+
+const person = {
+  name: 'Max',
+  greet: function greet (){
+    console.log('Hello there!');
+  }
+}
+
+person.greet();
