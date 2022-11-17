5e9100b953ce7fc602e100954bb4fb6345a7a19a
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-17 13:48:50 +0200

    Lesson 154

---
+++ section-7-working-with-dom/app.js
@@ -0,0 +1,18 @@
+const h1 = document.getElementById('main-title');
+
+h1.textContent = 'Some new content';
+h1.style.color = 'red';
+h1.style.backgroundColor = 'black';
+
+
+const li = document.querySelector('li:last-of-type');
+li.textContent = li.textContent + '(Changed!)';
+
+const body = document.body;
+
+// const listItemElements = document.querySelectorAll('li');
+const listItemElements = document.getElementsByTagName('li');
+
+for (const listItemEl of listItemElements){
+  console.dir(listItemEl);
+}
