b8cfe57fd9c7c036886b63769cfa337ae18d97a5
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-17 14:39:23 +0200

    Lesson 159

--- section-7-working-with-dom/app.js
+++ section-7-working-with-dom/app.js
@@ -1,18 +1,23 @@
-const h1 = document.getElementById('main-title');
+const ul = document.body.firstElementChild.nextElementSibling;
+const firstLi = ul.firstElementChild;
 
-h1.textContent = 'Some new content';
-h1.style.color = 'red';
-h1.style.backgroundColor = 'black';
+console.log(firstLi);
+
+// const h1 = document.getElementById('main-title');
+
+// h1.textContent = 'Some new content';
+// h1.style.color = 'red';
+// h1.style.backgroundColor = 'black';
 
 
-const li = document.querySelector('li:last-of-type');
-li.textContent = li.textContent + '(Changed!)';
+// const li = document.querySelector('li:last-of-type');
+// li.textContent = li.textContent + '(Changed!)';
 
-const body = document.body;
+// const body = document.body;
 
-// const listItemElements = document.querySelectorAll('li');
-const listItemElements = document.getElementsByTagName('li');
+// // const listItemElements = document.querySelectorAll('li');
+// const listItemElements = document.getElementsByTagName('li');
 
-for (const listItemEl of listItemElements){
-  console.dir(listItemEl);
-}
+// for (const listItemEl of listItemElements){
+//   console.dir(listItemEl);
+// }
