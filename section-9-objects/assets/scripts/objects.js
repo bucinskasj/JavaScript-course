07517f6d2858658da11c62418b78b0f62fc08c21
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-18 19:58:22 +0200

    Lesson 219

--- section-9-objects/assets/scripts/objects.js
+++ section-9-objects/assets/scripts/objects.js
@@ -3,10 +3,13 @@ const movieList = document.getElementById('movie-list');
 movieList.style['background-color'] = 'red';
 movieList.style.display = 'block';
 
+const userChosenKeyName = 'level';
+
 let person = {
   'first name': 'Max',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
+  [userChosenKeyName]: 'value',
   greet: function(){
     alert('Hi there!');
   },
@@ -18,6 +21,8 @@ delete person.age; //will detele attribute too
 // person.age = null;
 person.isAdmin = true;
 
-console.log(person['first name']);
+const keyName = 'first name';
+
+console.log(person[keyName]);
 console.log(person[1.5]);
 console.log(person);
