a225d711ea3c8ad878e04bd7c552a7ab1e1f5f31
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-18 13:18:01 +0200

    Lesson 198

--- section-8 - arrays-01-starting-code/app.js
+++ section-8 - arrays-01-starting-code/app.js
@@ -114,8 +114,6 @@ const sortedPrices = prices.sort((a, b) => {
 
 console.log(sortedPrices.reverse());
 
-const filteredArray = prices.filter((price, index, prices) => {
-  return price > 6;
-});
+const filteredArray = prices.filter(price => price > 6);
 
 console.log(filteredArray);
