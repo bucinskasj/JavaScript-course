1ada96ffdbdc2d8b0721fec137b3320f1316f3d6
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-18 12:11:28 +0200

    Lesson 190

--- section-8 - arrays-01-starting-code/app.js
+++ section-8 - arrays-01-starting-code/app.js
@@ -46,10 +46,14 @@
 // console.log(hobbies);
 // console.log(removedElements);
 
-const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
 // const storedResults = testResults.slice(2);
 const storedResults = testResults.concat([3.99, 2]);
 
 testResults.push(5.91);
 
 console.log(storedResults, testResults);
+console.log(testResults.lastIndexOf(1.5));
+
+const personData = [{name: 'Max'}, {name: 'Manuel'}];
+console.log(personData.indexOf({name: 'Manuel'})); // won't work on objects
