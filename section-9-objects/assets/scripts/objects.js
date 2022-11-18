f564591fd87ed4bbc02ff44777529a9ebf6102d9
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-18 19:48:31 +0200

    Lesson 217

--- section-9-objects/assets/scripts/objects.js
+++ section-9-objects/assets/scripts/objects.js
@@ -1,5 +1,10 @@
+const movieList = document.getElementById('movie-list');
+
+movieList.style['background-color'] = 'red';
+movieList.style.display = 'block';
+
 let person = {
-  name: 'Max',
+  'first name': 'Max',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
   greet: function(){
@@ -9,7 +14,7 @@ let person = {
 
 delete person.age; //will detele attribute too
 // person.age = undefined; //better use null. Devs never assign undefined.
-person.age = null;
+// person.age = null;
 person.isAdmin = true;
 
-console.log(person);
+console.log(person['first name']);
