9a1931f5285daaac50dd4b6c762be242777d5532
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-19 09:37:23 +0200

    Lesson 223

--- section-9-objects/assets/scripts/objects.js
+++ section-9-objects/assets/scripts/objects.js
@@ -1,27 +1,31 @@
 const addMovieBtn = document.getElementById("add-movie-btn");
-const searchBtn = document.getElementById("search-bth");
+const searchBtn = document.getElementById("search-btn");
 
 const movies = [];
 
-const renderMovies = () => {
-  const movieList = document.getElementById('movie-list');
-  
+const renderMovies = (filter = "") => {
+  const movieList = document.getElementById("movie-list");
+
   if (movies.length === 0) {
-    movieList.classList.remove('visible');
+    movieList.classList.remove("visible");
     return;
   } else {
-    movieList.classList.add('visible');
+    movieList.classList.add("visible");
   }
-  movieList.innerHTML = '';
+  movieList.innerHTML = "";
 
-  movies.forEach((movie) => {
-    const movieEl = document.createElement('li');
-    let text = movie.info.title + ' - ';
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter(movie => movie.info.title.includes(filter));
+
+  filteredMovies.forEach(movie => {
+    const movieEl = document.createElement("li");
+    let text = movie.info.title + " - ";
     for (const key in movie.info) {
-      if (key !== 'title') {
+      if (key !== "title") {
         text = text + `${key}: ${movie.info[key]}`;
       }
-    };
+    }
     movieEl.textContent = text;
     movieList.append(movieEl);
   });
@@ -43,14 +47,19 @@ const addMovieHandler = () => {
   const newMovie = {
     info: {
       title,
-      [extraName]: extraValue
+      [extraName]: extraValue,
     },
-    id: Math.random()
+    id: Math.random(),
   };
 
   movies.push(newMovie);
   renderMovies();
 };
 
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById("filter-title").value;
+  renderMovies(filterTerm);
+};
 
-addMovieBtn.addEventListener('click', addMovieHandler);
+addMovieBtn.addEventListener("click", addMovieHandler);
+searchBtn.addEventListener("click", searchMovieHandler);
