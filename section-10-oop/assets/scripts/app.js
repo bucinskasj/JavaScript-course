450443c26e26a9b20f302234a494ffeefa54a29a
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-19 14:52:08 +0200

    Lesson 24

--- section-10-oop/assets/scripts/app.js
+++ section-10-oop/assets/scripts/app.js
@@ -1,36 +1,41 @@
 class Product {
- title = 'DEFAULT';
- imageUrl;
- description;
- price;
+  title = "DEFAULT";
+  imageUrl;
+  description;
+  price;
+
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
 }
 
-console.log(new Product());
-
+// console.log(new Product());
 
 const productList = {
   products: [
-    // new Product()
-    {
-      title:'A Pillow',
-      imageUrl: 'https://unitedpillow.com/images/companies/2/pillow-thumb.jpg?1564370699038',
-      price: 19.99,
-      description: 'A soft pillow!'
-    },
-    {
-      title:'A Carpet on the wall',
-      imageUrl: 'https://mf.b37mrtl.ru/rbthmedia/images/web/in-rbth/images/2014-01/big/9.01/2860348_468.jpg',
-      price: 89.99,
-      description: 'Perfect carpet for your wall'
-    }
+    new Product(
+      "A Pillow",
+      "https://unitedpillow.com/images/companies/2/pillow-thumb.jpg?1564370699038",
+      "A soft pillow!",
+      19.99
+    ),
+    new Product(
+      "A Carpet on the wall",
+      "https://mf.b37mrtl.ru/rbthmedia/images/web/in-rbth/images/2014-01/big/9.01/2860348_468.jpg",
+      "Perfect carpet for your wall",
+      89.99
+    )
   ],
   render() {
-    const renderHook = document.getElementById('app');
-    const prodList = document.createElement('ul');
-    prodList.className = 'product-list';
-    for(const prod of this.products) {
-      const prodEl = document.createElement('li');
-      prodEl.className = 'product-item';
+    const renderHook = document.getElementById("app");
+    const prodList = document.createElement("ul");
+    prodList.className = "product-list";
+    for (const prod of this.products) {
+      const prodEl = document.createElement("li");
+      prodEl.className = "product-item";
       prodEl.innerHTML = `
         <div>
         <img src="${prod.imageUrl}" alt="${prod.title}">
@@ -45,7 +50,7 @@ const productList = {
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
-  }
+  },
 };
 
 productList.render();
