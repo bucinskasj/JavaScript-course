f3bac02a2d5178f2be7bc12cbc4efee120838cef
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-20 20:58:54 +0200

    Lesson 297

--- section-13-dv-dom-sizes-html/assets/scripts/analytics.js
+++ section-13-dv-dom-sizes-html/assets/scripts/analytics.js
@@ -1,1 +1,7 @@
-console.log('Sending some analytics...');
+const intervalId = setInterval(() => {
+  console.log('Sending analytics')
+}, 2000); /// can pass in the array as well
+
+document.getElementById("stop-analytics-btn").addEventListener("click", () => {
+  clearInterval(intervalId);
+});
