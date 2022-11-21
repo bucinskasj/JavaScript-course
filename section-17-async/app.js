c9e5dce3811f6333f45fa1e2e84155526b40d27a
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-21 20:11:37 +0200

    Lesson 353

--- section-17-async/app.js
+++ section-17-async/app.js
@@ -2,12 +2,19 @@ const button = document.querySelector('button');
 const output = document.querySelector('p');
 
 function trackUserHandler() {
-  navigator.geolocation.getCurrentPosition(posData => {
-    console.log(posData)
-  }, error => {
-    console.log(error);
-  },);
-
+  navigator.geolocation.getCurrentPosition(
+    posData => {
+      setTimeout(() => {
+        console.log(posData);
+      }, 2000);
+    },
+    error => {
+      console.log(error);
+    },
+  );
+  setTimeout(() => {
+    console.log('Timer done!')
+  }, 0);
   console.log('Getting position...');
 }
 
