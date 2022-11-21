22a653aef82e07411c8e5fda84bd653d257a1d3b
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-21 10:15:31 +0200

    Lesson 314

--- section-14- events/assets/scripts/events.js
+++ section-14- events/assets/scripts/events.js
@@ -50,3 +50,19 @@ button.addEventListener('mouseenter', event => {
   console.log('Clicked BUTTON');
   console.log(event);
 });
+
+
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+// listItems.forEach(listItem => {
+//   listItem.addEventListener('click', event => {
+//     event.target.classList.toggle('highlight');
+//   });
+// });
+
+list.addEventListener('click', event => {
+  // console.log(event.currentTarget);
+  // event.target.classList.toggle('highlight');
+  event.target.closest('li').classList.toggle('highlight');
+ });
